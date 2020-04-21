d8c79e3855fb75dd5820d8d740377b690cf0320a
Flora Dupont <flora.dupont@hotmail.fr>
2020-04-21 16:01:58 +0200

    ajout projet stockapi

---
+++ microservices-api/spring-reactive-stockapi/src/main/java/com/inti/formation/SpringReactiveStockapiApplication.java
@@ -0,0 +1,13 @@
+package com.inti.formation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveStockapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveStockapiApplication.class, args);
+	}
+
+}
