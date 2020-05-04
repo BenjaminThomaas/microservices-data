4d6a2cff09c9c7484dd36e59b6d91da7558ce92a
Flora Dupont <flora.dupont@hotmail.fr>
2020-05-04 17:14:41 +0200

    deplace consummer

---
+++ microservices-consumer/src/main/java/com/inti/formation/MicroservicesConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.inti.formation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesConsumerApplication.class, args);
+	}
+
+}
--- microservices-consumer/src/test/java/com/inti/formation/MicroservicesConsumerApplicationTests.java
+++ microservices-consumer/src/test/java/com/inti/formation/MicroservicesConsumerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MicroserviceConsumerApplicationTests {
+class MicroservicesConsumerApplicationTests {
 
 	@Test
 	void contextLoads() {
